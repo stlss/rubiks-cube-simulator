b8715df114dbbec536b355ff215ea6b7ba5958ae
stlss <a.ivakin2014@gmail.com>
2025-08-02 18:23:54 +0500

    [feature/cube-building] add value objects for Rubik's cube

---
+++ RubiksCubeSimulator.Domain/ValueObjects/RubiksCube/RubiksCubeColor.cs
@@ -0,0 +1,11 @@
+﻿namespace RubiksCubeSimulator.Domain.ValueObjects.RubiksCube;
+
+public enum RubiksCubeColor
+{
+    White,
+    Blue,
+    Red,
+    Yellow,
+    Green,
+    Orange,
+}
---
+++ RubiksCubeSimulator.Domain/ValueObjects/RubiksCube/RubiksCubeFace.cs
@@ -0,0 +1,5 @@
+﻿using System.Collections.Immutable;
+
+namespace RubiksCubeSimulator.Domain.ValueObjects.RubiksCube;
+
+public sealed record RubiksCubeFace(ImmutableArray<ImmutableArray<RubiksCubeFace>> StickerColors);
