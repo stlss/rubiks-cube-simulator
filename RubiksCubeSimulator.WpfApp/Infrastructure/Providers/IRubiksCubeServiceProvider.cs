a67e54f580ef77e287cc959a90b19f3e9f8c4fc1
stlss <a.ivakin2014@gmail.com>
2025-08-03 23:02:02 +0500

    [feature/cube-building] add Rubik's cube service provider

---
+++ RubiksCubeSimulator.WpfApp/Infrastructure/Providers/IRubiksCubeServiceProvider.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RubiksCubeSimulator.Application;
+using RubiksCubeSimulator.Domain.Services;
+
+namespace RubiksCubeSimulator.WpfApp.Infrastructure.Providers;
+
+internal interface IRubiksCubeServiceProvider
+{
+    public IRubiksCubeBuilder GetRubiksCubeBuilder();
+}
+
+internal sealed class RubiksCubeServiceProvider : IRubiksCubeServiceProvider
+{
+    private readonly ServiceProvider _serviceProvider = CreateServiceProvider();
+
+    private static ServiceProvider CreateServiceProvider()
+    {
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+
+        var provider = services.BuildServiceProvider();
+        return provider;
+    }
+
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSingleton<IRubiksCubeBuilder, RubiksCubeBuilder>();
+    }
+
+    public IRubiksCubeBuilder GetRubiksCubeBuilder() => _serviceProvider.GetRequiredService<IRubiksCubeBuilder>();
+}
--- RubiksCubeSimulator.WpfApp/ViewModels/Windows/MainWindowViewModel.cs
+++ RubiksCubeSimulator.WpfApp/ViewModels/Windows/MainWindowViewModel.cs
@@ -1,7 +1,19 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using RubiksCubeSimulator.Domain.Services;
+using RubiksCubeSimulator.WpfApp.Infrastructure.Providers;
 
 namespace RubiksCubeSimulator.WpfApp.ViewModels.Windows;
 
 internal sealed class MainWindowViewModel : ObservableObject
 {
+    private readonly IRubiksCubeBuilder _cubeBuilder;
+
+    public MainWindowViewModel() : this(new RubiksCubeServiceProvider())
+    {
+    }
+
+    private MainWindowViewModel(IRubiksCubeServiceProvider serviceProvider)
+    {
+        _cubeBuilder = serviceProvider.GetRubiksCubeBuilder();
+    }
 }
