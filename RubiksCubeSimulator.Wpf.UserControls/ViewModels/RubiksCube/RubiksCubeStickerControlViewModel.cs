a29202907a608b9d38dfe6e5e6bf6ed3df291e2b
stlss <a.ivakin2014@gmail.com>
2025-08-07 23:26:47 +0500

    [feature/cube-building] add Rubik's cube sticker control

---
+++ RubiksCubeSimulator.Wpf.UserControls/ViewModels/RubiksCube/RubiksCubeStickerControlViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Media;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace RubiksCubeSimulator.Wpf.UserControls.ViewModels.RubiksCube;
+
+public sealed class RubiksCubeStickerControlViewModel : ObservableObject
+{
+    private SolidColorBrush _stickerColorBrush = new(Colors.DarkGray);
+
+    public SolidColorBrush StickerColorBrush
+    {
+        get => _stickerColorBrush;
+        set => SetProperty(ref _stickerColorBrush, value);
+    }
+}
---
+++ RubiksCubeSimulator.Wpf.UserControls/Views/RubiksCube/RubiksCubeStickerControl.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace RubiksCubeSimulator.Wpf.UserControls.Views.RubiksCube;
+
+public sealed partial class RubiksCubeStickerControl : UserControl
+{
+    public RubiksCubeStickerControl()
+    {
+        InitializeComponent();
+    }
+}
