e170d36334147c0570ab7c159615e08db2d1a968
stlss <a.ivakin2014@gmail.com>
2025-08-07 23:27:02 +0500

    [feature/cube-building] add Rubik's cube face control

---
+++ RubiksCubeSimulator.Wpf.UserControls/ViewModels/RubiksCube/RubiksCubeFaceControlViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace RubiksCubeSimulator.Wpf.UserControls.ViewModels.RubiksCube;
+
+public sealed class RubiksCubeFaceControlViewModel
+{
+    private const int DefaultDimension = 3;
+
+    public int Dimension { get; init; } = DefaultDimension;
+
+    public Thickness BorderThickness => new(1.5 * DefaultDimension / Dimension);
+
+    public IReadOnlyList<RubiksCubeStickerControlViewModel> StickerControlViewModels { get; init; } =
+        Enumerable.Repeat(new RubiksCubeStickerControlViewModel(), 25).ToList();
+}
