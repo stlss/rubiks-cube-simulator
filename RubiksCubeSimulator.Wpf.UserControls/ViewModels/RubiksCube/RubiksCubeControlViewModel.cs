bd1a975a9a7562eb19bfdd2451d7e482d6ce8e77
stlss <a.ivakin2014@gmail.com>
2025-08-07 23:28:56 +0500

    [feature/cube-building] add Rubik's cube control

---
+++ RubiksCubeSimulator.Wpf.UserControls/ViewModels/RubiksCube/RubiksCubeControlViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RubiksCubeSimulator.Wpf.UserControls.ViewModels.RubiksCube;
+
+public sealed class RubiksCubeControlViewModel
+{
+    public RubiksCubeFaceControlViewModel UpFaceControlViewModel { get; init; } = new();
+
+    public RubiksCubeFaceControlViewModel RightFaceControlViewModel { get; init; } = new();
+
+    public RubiksCubeFaceControlViewModel FrontFaceControlViewModel { get; init; } = new();
+}
---
+++ RubiksCubeSimulator.Wpf.UserControls/Views/RubiksCube/RubiksCubeControl.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace RubiksCubeSimulator.Wpf.UserControls.Views.RubiksCube;
+
+public partial class RubiksCubeControl : UserControl
+{
+    public RubiksCubeControl()
+    {
+        InitializeComponent();
+    }
+}
