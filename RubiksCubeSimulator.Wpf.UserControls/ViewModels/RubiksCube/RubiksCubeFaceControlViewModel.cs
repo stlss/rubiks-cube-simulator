7e6a5a3dbd9eca9cf2a6794ef9f9566b342581bb
stlss <a.ivakin2014@gmail.com>
2025-08-08 00:28:29 +0500

    [feature/cube-building] refactoring RubiksCubeFaceControlViewModel

--- RubiksCubeSimulator.Wpf.UserControls/ViewModels/RubiksCube/RubiksCubeFaceControlViewModel.cs
+++ RubiksCubeSimulator.Wpf.UserControls/ViewModels/RubiksCube/RubiksCubeFaceControlViewModel.cs
@@ -6,10 +6,10 @@ public sealed class RubiksCubeFaceControlViewModel
 {
     private const int DefaultDimension = 3;
 
-    public int Dimension { get; init; } = DefaultDimension;
+    public IReadOnlyList<RubiksCubeStickerControlViewModel> StickerControlViewModels { get; init; } =
+        Enumerable.Repeat(new RubiksCubeStickerControlViewModel(), DefaultDimension * DefaultDimension).ToList();
+
+    public int Dimension => (int)Math.Sqrt(StickerControlViewModels.Count);
 
     public Thickness BorderThickness => new(1.5 * DefaultDimension / Dimension);
-
-    public IReadOnlyList<RubiksCubeStickerControlViewModel> StickerControlViewModels { get; init; } =
-        Enumerable.Repeat(new RubiksCubeStickerControlViewModel(), 25).ToList();
 }
