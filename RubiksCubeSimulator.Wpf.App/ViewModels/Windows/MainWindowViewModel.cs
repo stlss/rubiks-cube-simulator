07cda38165daf50d7586f25993a98f3af7f43d17
stlss <a.ivakin2014@gmail.com>
2025-08-07 23:31:04 +0500

    [feature/cube-building] add Rubik's cube control to main window

--- RubiksCubeSimulator.Wpf.App/ViewModels/Windows/MainWindowViewModel.cs
+++ RubiksCubeSimulator.Wpf.App/ViewModels/Windows/MainWindowViewModel.cs
@@ -1,12 +1,15 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using RubiksCubeSimulator.Domain.Services;
 using RubiksCubeSimulator.Wpf.App.Infrastructure.Providers;
+using RubiksCubeSimulator.Wpf.UserControls.ViewModels.RubiksCube;
 
 namespace RubiksCubeSimulator.Wpf.App.ViewModels.Windows;
 
 internal sealed class MainWindowViewModel : ObservableObject
 {
     private readonly IRubiksCubeBuilder _cubeBuilder;
+
+    private RubiksCubeControlViewModel _cubeControlViewModel = new();
 
     public MainWindowViewModel() : this(new RubiksCubeServiceProvider())
     {
@@ -15,5 +18,11 @@ internal sealed class MainWindowViewModel : ObservableObject
     private MainWindowViewModel(IRubiksCubeServiceProvider serviceProvider)
     {
         _cubeBuilder = serviceProvider.GetRubiksCubeBuilder();
+    }
+
+    public RubiksCubeControlViewModel CubeControlViewModel
+    {
+        get => _cubeControlViewModel;
+        set => SetProperty(ref _cubeControlViewModel, value);
     }
 }
