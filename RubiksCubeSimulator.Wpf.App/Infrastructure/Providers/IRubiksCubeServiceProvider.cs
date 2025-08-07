12c4b8d477d2f3d897610ec628a26f7a89a92a1d
stlss <a.ivakin2014@gmail.com>
2025-08-08 01:32:57 +0500

    [feature/cube-building] add getting of Rubik's cube control view model builder to Rubik's cube service provider

--- RubiksCubeSimulator.Wpf.App/Infrastructure/Providers/IRubiksCubeServiceProvider.cs
+++ RubiksCubeSimulator.Wpf.App/Infrastructure/Providers/IRubiksCubeServiceProvider.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using RubiksCubeSimulator.Application;
 using RubiksCubeSimulator.Domain.Services;
+using RubiksCubeSimulator.Wpf.App.Infrastructure.ViewModelBuilders.RubiksCubeControls;
 
 namespace RubiksCubeSimulator.Wpf.App.Infrastructure.Providers;
 
 internal interface IRubiksCubeServiceProvider
 {
     public IRubiksCubeBuilder GetRubiksCubeBuilder();
+
+    public IRubiksCubeControlViewModelBuilder GetRubiksCubeControlViewModelBuilder();
 }
 
 internal sealed class RubiksCubeServiceProvider : IRubiksCubeServiceProvider
@@ -25,7 +28,15 @@ internal sealed class RubiksCubeServiceProvider : IRubiksCubeServiceProvider
     private static void ConfigureServices(IServiceCollection services)
     {
         services.AddSingleton<IRubiksCubeBuilder, RubiksCubeBuilder>();
+
+        services.AddSingleton<IRubiksCubeStickerControlViewModelBuilder, RubiksCubeStickerControlViewModelBuilder>();
+        services.AddSingleton<IRubiksCubeFaceControlViewModelBuilder, RubiksCubeFaceControlViewModelBuilder>();
+        services.AddSingleton<IRubiksCubeControlViewModelBuilder, RubiksCubeControlViewModelBuilder>();
     }
 
-    public IRubiksCubeBuilder GetRubiksCubeBuilder() => _serviceProvider.GetRequiredService<IRubiksCubeBuilder>();
+    public IRubiksCubeBuilder GetRubiksCubeBuilder()
+        => _serviceProvider.GetRequiredService<IRubiksCubeBuilder>();
+
+    public IRubiksCubeControlViewModelBuilder GetRubiksCubeControlViewModelBuilder()
+        => _serviceProvider.GetRequiredService<IRubiksCubeControlViewModelBuilder>();
 }
