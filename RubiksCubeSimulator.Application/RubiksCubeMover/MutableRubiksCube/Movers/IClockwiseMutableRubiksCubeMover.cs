dc9dc9a57188926da9de0cc2b753523a50a8ee84
stlss <a.ivakin2014@gmail.com>
2025-08-10 22:53:02 +0500

    [feature/cube-moving] add interfaces of clockwise/counterclockwise mutable cube movers

---
+++ RubiksCubeSimulator.Application/RubiksCubeMover/MutableRubiksCube/Movers/IClockwiseMutableRubiksCubeMover.cs
@@ -0,0 +1,17 @@
+﻿namespace RubiksCubeSimulator.Application.RubiksCubeMover.MutableRubiksCube.Movers;
+
+public interface IClockwiseMutableRubiksCubeMover
+{
+    public MutableRubiksCube MoveMutableRubiksCubeUp(MutableRubiksCube cube, int sliceNumber);
+
+    public MutableRubiksCube MoveMutableRubiksCubeRight(MutableRubiksCube cube, int sliceNumber);
+
+    public MutableRubiksCube MoveMutableRubiksCubeFront(MutableRubiksCube cube, int sliceNumber);
+
+
+    public MutableRubiksCube MoveMutableRubiksCubeDown(MutableRubiksCube cube, int sliceNumber);
+
+    public MutableRubiksCube MoveMutableRubiksCubeLeft(MutableRubiksCube cube, int sliceNumber);
+
+    public MutableRubiksCube MoveMutableRubiksCubeBack(MutableRubiksCube cube, int sliceNumber);
+}
