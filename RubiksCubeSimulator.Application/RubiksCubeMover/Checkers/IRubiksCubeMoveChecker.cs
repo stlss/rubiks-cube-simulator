cf2ec25c4b6edb8ce6c4d63276a9e64680a40a4e
stlss <a.ivakin2014@gmail.com>
2025-08-10 21:59:29 +0500

    [feature/cube-moving] add cube move checker

---
+++ RubiksCubeSimulator.Application/RubiksCubeMover/Checkers/IRubiksCubeMoveChecker.cs
@@ -0,0 +1,30 @@
+﻿using RubiksCubeSimulator.Domain.ValueObjects.RubiksCube.Moves;
+
+namespace RubiksCubeSimulator.Application.RubiksCubeMover.Checkers;
+
+public interface IRubiksCubeMoveChecker
+{
+    public bool IsCorrectRubiksCubeSliceMove(RubiksCubeSliceMove move, int cubeDimension);
+
+    public bool IsCorrectWholeRubiksCubeMove(WholeRubiksCubeMove move);
+}
+
+internal sealed class RubiksCubeMoveChecker : IRubiksCubeMoveChecker
+{
+    public bool IsCorrectRubiksCubeSliceMove(RubiksCubeSliceMove move, int cubeDimension)
+    {
+        var isCorrectFace = Enum.IsDefined(move.Face);
+        var isCorrectDirection = Enum.IsDefined(move.Direction);
+        var isCorrectSlice = 0 <= move.Slice && move.Slice < cubeDimension;
+
+        return isCorrectFace && isCorrectDirection && isCorrectSlice;
+    }
+
+    public bool IsCorrectWholeRubiksCubeMove(WholeRubiksCubeMove move)
+    {
+        var isCorrectAxis = Enum.IsDefined(move.Axis);
+        var isCorrectDirection = Enum.IsDefined(move.Direction);
+
+        return isCorrectAxis && isCorrectDirection;
+    }
+}
