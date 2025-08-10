8ac80b84b67a225f35de29775514c4945cec5556
stlss <a.ivakin2014@gmail.com>
2025-08-10 21:42:44 +0500

    [feature/cube-moving] add interface of cube mover

---
+++ RubiksCubeSimulator.Application/RubiksCubeMover/RubiksCubeMover.cs
@@ -0,0 +1,18 @@
+﻿using RubiksCubeSimulator.Domain.Services;
+using RubiksCubeSimulator.Domain.ValueObjects.RubiksCube;
+using RubiksCubeSimulator.Domain.ValueObjects.RubiksCube.Moves;
+
+namespace RubiksCubeSimulator.Application.RubiksCubeMover;
+
+public sealed class RubiksCubeMover : IRubiksCubeMover
+{
+    public RubiksCube MoveRubiksCube(RubiksCube cube, RubiksCubeMoveBase move)
+    {
+        throw new NotImplementedException();
+    }
+
+    public RubiksCube MoveRubiksCube(RubiksCube cube, IEnumerable<RubiksCubeMoveBase> move)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ RubiksCubeSimulator.Domain/Services/IRubiksCubeMover.cs
@@ -0,0 +1,11 @@
+﻿using RubiksCubeSimulator.Domain.ValueObjects.RubiksCube;
+using RubiksCubeSimulator.Domain.ValueObjects.RubiksCube.Moves;
+
+namespace RubiksCubeSimulator.Domain.Services;
+
+public interface IRubiksCubeMover
+{
+    public RubiksCube MoveRubiksCube(RubiksCube cube, RubiksCubeMoveBase move);
+
+    public RubiksCube MoveRubiksCube(RubiksCube cube, IEnumerable<RubiksCubeMoveBase> move);
+}
