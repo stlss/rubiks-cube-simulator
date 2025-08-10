705e60ab3aaf2b0af0d10f4fa620a6a85d5a5fed
stlss <a.ivakin2014@gmail.com>
2025-08-10 23:22:01 +0500

    [feature/cube-moving] add implementation of counterclockwise mutable cube mover

--- RubiksCubeSimulator.Application/RubiksCubeMover/MutableRubiksCube/Movers/ICounterclockwiseMutableRubiksCubeMover.cs
+++ RubiksCubeSimulator.Application/RubiksCubeMover/MutableRubiksCube/Movers/ICounterclockwiseMutableRubiksCubeMover.cs
@@ -15,3 +15,60 @@ public interface ICounterclockwiseMutableRubiksCubeMover
 
     public MutableRubiksCube MoveMutableRubiksCubeBack(MutableRubiksCube cube, int sliceNumber);
 }
+
+internal sealed class CounterclockwiseMutableRubiksCubeMover(
+    IClockwiseMutableRubiksCubeMover clockwiseMutableCubeMover)
+    : ICounterclockwiseMutableRubiksCubeMover
+{
+    public MutableRubiksCube MoveMutableRubiksCubeUp(MutableRubiksCube cube, int sliceNumber)
+    {
+        var reversedSliceNumber = GetReversedSliceNumber(sliceNumber, cube.Dimension);
+        var movedCube = clockwiseMutableCubeMover.MoveMutableRubiksCubeDown(cube, reversedSliceNumber);
+
+        return movedCube;
+    }
+
+    public MutableRubiksCube MoveMutableRubiksCubeRight(MutableRubiksCube cube, int sliceNumber)
+    {
+        var reversedSliceNumber = GetReversedSliceNumber(sliceNumber, cube.Dimension);
+        var movedCube = clockwiseMutableCubeMover.MoveMutableRubiksCubeLeft(cube, reversedSliceNumber);
+
+        return movedCube;
+    }
+
+    public MutableRubiksCube MoveMutableRubiksCubeFront(MutableRubiksCube cube, int sliceNumber)
+    {
+        var reversedSliceNumber = GetReversedSliceNumber(sliceNumber, cube.Dimension);
+        var movedCube = clockwiseMutableCubeMover.MoveMutableRubiksCubeBack(cube, reversedSliceNumber);
+
+        return movedCube;
+    }
+
+
+    public MutableRubiksCube MoveMutableRubiksCubeDown(MutableRubiksCube cube, int sliceNumber)
+    {
+        var reversedSliceNumber = GetReversedSliceNumber(sliceNumber, cube.Dimension);
+        var movedCube = clockwiseMutableCubeMover.MoveMutableRubiksCubeUp(cube, reversedSliceNumber);
+
+        return movedCube;
+    }
+
+    public MutableRubiksCube MoveMutableRubiksCubeLeft(MutableRubiksCube cube, int sliceNumber)
+    {
+        var reversedSliceNumber = GetReversedSliceNumber(sliceNumber, cube.Dimension);
+        var movedCube = clockwiseMutableCubeMover.MoveMutableRubiksCubeRight(cube, reversedSliceNumber);
+
+        return movedCube;
+    }
+
+    public MutableRubiksCube MoveMutableRubiksCubeBack(MutableRubiksCube cube, int sliceNumber)
+    {
+        var reversedSliceNumber = GetReversedSliceNumber(sliceNumber, cube.Dimension);
+        var movedCube = clockwiseMutableCubeMover.MoveMutableRubiksCubeFront(cube, reversedSliceNumber);
+
+        return movedCube;
+    }
+
+
+    private static int GetReversedSliceNumber(int sliceNumber, int cubeDimension) => cubeDimension - sliceNumber - 1;
+}
