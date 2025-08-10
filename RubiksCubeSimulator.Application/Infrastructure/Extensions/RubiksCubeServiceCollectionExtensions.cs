9a06a326991348d962b1a40fc44a7b405dc4ade8
stlss <a.ivakin2014@gmail.com>
2025-08-10 20:12:48 +0500

    [feature/cube-building] add adding of cube builder in DI

---
+++ RubiksCubeSimulator.Application/Infrastructure/Extensions/RubiksCubeServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RubiksCubeSimulator.Domain.Services;
+
+namespace RubiksCubeSimulator.Application.Infrastructure.Extensions;
+
+public static class RubiksCubeServiceCollectionExtensions
+{
+    public static IServiceCollection AddRubiksCubeBuilder(this IServiceCollection services)
+    {
+        services.AddSingleton<IRubiksCubeBuilder, RubiksCubeBuilder.RubiksCubeBuilder>();
+
+        return services;
+    }
+}
