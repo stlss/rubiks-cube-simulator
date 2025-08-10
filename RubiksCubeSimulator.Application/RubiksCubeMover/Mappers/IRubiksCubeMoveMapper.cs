1f61c22efe732282772c98443d069cb400e27642
stlss <a.ivakin2014@gmail.com>
2025-08-10 21:55:40 +0500

    [feature/cube-moving] add cube move mapper

---
+++ RubiksCubeSimulator.Application/RubiksCubeMover/Mappers/IRubiksCubeMoveMapper.cs
@@ -0,0 +1,34 @@
+﻿using RubiksCubeSimulator.Domain.ValueObjects.RubiksCube.Moves;
+using RubiksCubeSimulator.Domain.ValueObjects.RubiksCube.Moves.Enums;
+
+namespace RubiksCubeSimulator.Application.RubiksCubeMover.Mappers;
+
+public interface IRubiksCubeMoveMapper
+{
+    public IReadOnlyList<RubiksCubeSliceMove> Map(WholeRubiksCubeMove move, int cubeDimension);
+}
+
+internal sealed class RubiksCubeMoveMapper : IRubiksCubeMoveMapper
+{
+    private static readonly Dictionary<AxisName, MoveFace> FaceMoveByAxisName = new()
+    {
+        [AxisName.X] = MoveFace.Right,
+        [AxisName.Y] = MoveFace.Up,
+        [AxisName.Z] = MoveFace.Front
+    };
+
+    public IReadOnlyList<RubiksCubeSliceMove> Map(WholeRubiksCubeMove move, int cubeDimension)
+    {
+        var sliceMoves = new List<RubiksCubeSliceMove>(cubeDimension);
+
+        var faceMove = FaceMoveByAxisName[move.Axis];
+        var directionMove = move.Direction;
+
+        for (var sliceNumber = 0; sliceNumber < cubeDimension; sliceNumber++)
+        {
+            sliceMoves.Add(new RubiksCubeSliceMove(faceMove, directionMove, sliceNumber));
+        }
+
+        return sliceMoves;
+    }
+}
