df3b900836cc36963d03f1c63e64c10deb505fdd
stlss <a.ivakin2014@gmail.com>
2025-08-17 02:53:20 +0500

    [feature/cube-moving] add opposite move direction provider

---
+++ RubiksCubeSimulator.UnitTests/Infrastructure/RubiksCubeProviders/OppositeMoveDirectionProvider.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using RubiksCubeSimulator.Domain.ValueObjects.RubiksCube;
+using RubiksCubeSimulator.Domain.ValueObjects.RubiksCube.Moves.Enums;
+
+namespace RubiksCubeSimulator.UnitTests.Infrastructure.RubiksCubeProviders;
+
+internal static class OppositeMoveDirectionProvider
+{
+    public static MoveDirection GetOppositeMoveDirection(MoveDirection moveDirection)
+    {
+        return moveDirection switch
+        {
+            MoveDirection.Clockwise => MoveDirection.CounterClockwise,
+            MoveDirection.CounterClockwise => MoveDirection.Clockwise,
+
+            _ => throw new InvalidEnumArgumentException(
+                nameof(moveDirection),
+                (int)moveDirection,
+                typeof(MoveDirection)),
+        };
+    }
+}
