80b05247f7745ea525467ec4db57399ec2b283eb
stlss <a.ivakin2014@gmail.com>
2025-08-26 20:31:29 +0500

    [feature/cube-moving] add relative mouse move behavior

---
+++ RubiksCubeSimulator.Wpf.UserControls/Views/Behaviors/RelativeMouseMoveBehavior.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using Microsoft.Xaml.Behaviors;
+
+namespace RubiksCubeSimulator.Wpf.UserControls.Views.Behaviors;
+
+internal sealed class RelativeMouseMoveBehavior : Behavior<FrameworkElement>
+{
+    public event EventHandler<RelativeMouseEventArgs>? RelativeMouseMove;
+
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+        AssociatedObject.MouseMove += OnMouseMove;
+        AssociatedObject.MouseLeave += OnMouseLeave;
+    }
+
+    protected override void OnDetaching()
+    {
+        AssociatedObject.MouseMove -= OnMouseMove;
+        AssociatedObject.MouseLeave -= OnMouseLeave;
+    }
+
+    private void OnMouseMove(object sender, MouseEventArgs e)
+    {
+        var frameworkElement = AssociatedObject;
+        var point = e.GetPosition(frameworkElement);
+
+        double width = frameworkElement.ActualWidth;
+        double height = frameworkElement.ActualHeight;
+
+        var relativeMousePosition = new Point(
+            width > 0 ? point.X / width : 0,
+            height > 0 ? point.Y / height : 0
+        );
+
+        InvokeMouseMoveEvent(relativeMousePosition);
+    }
+
+    private void OnMouseLeave(object sender, MouseEventArgs e)
+    {
+        InvokeMouseMoveEvent(null);
+    }
+
+    private void InvokeMouseMoveEvent(Point? relativeMousePosition)
+    {
+        var eventArgs = new RelativeMouseEventArgs { RelativeMousePosition = relativeMousePosition };
+        RelativeMouseMove?.Invoke(this, eventArgs);
+    }
+}
+
+public sealed class RelativeMouseEventArgs : EventArgs
+{
+    public required Point? RelativeMousePosition { get; init; }
+}
