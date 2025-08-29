a202f0c020d4f6c54869786455126cc8c1dfb8ec
stlss <a.ivakin2014@gmail.com>
2025-08-29 19:06:42 +0500

    [feature/cube-moving] add key events in main window view and view model

--- RubiksCubeSimulator.Wpf.App/ViewModels/Windows/MainWindowViewModel.cs
+++ RubiksCubeSimulator.Wpf.App/ViewModels/Windows/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using RubiksCubeSimulator.Domain.Services;
 using RubiksCubeSimulator.Wpf.App.Infrastructure.RubiksCubeControlViewModelBuilders;
 using RubiksCubeSimulator.Wpf.App.Infrastructure.RubiksCubeServiceProvider;
@@ -19,6 +21,7 @@ internal sealed class MainWindowViewModel : ObservableObject
         private set => SetProperty(ref _cubeControlViewModel, value);
     }
 
+
     public MainWindowViewModel() : this(new RubiksCubeServiceProvider())
     {
     }
@@ -28,12 +31,25 @@ internal sealed class MainWindowViewModel : ObservableObject
         _cubeBuilder = serviceProvider.GetCubeBuilder();
         _cubeViewModelBuilder = serviceProvider.GetCubeViewModelBuilder();
 
+        KeyDownCommand = new RelayCommand<KeyEventArgs>(e => KeyDown?.Invoke(this, e));
+        KeyUpCommand = new RelayCommand<KeyEventArgs>(e => KeyUp?.Invoke(this, e));
+
         SetCubeViewModel();
     }
 
+
     private void SetCubeViewModel()
     {
-        var cube = _cubeBuilder.BuildSolvedRubiksCube(3);
+        var cube = _cubeBuilder.BuildSolvedRubiksCube(2);
         CubeControlViewModel = _cubeViewModelBuilder.Build(cube);
     }
+
+
+    public event KeyEventHandler? KeyDown;
+    public event KeyEventHandler? KeyUp;
+
+
+    public IRelayCommand<KeyEventArgs>? KeyDownCommand { get; private init; }
+
+    public IRelayCommand<KeyEventArgs>? KeyUpCommand { get; private init; }
 }
