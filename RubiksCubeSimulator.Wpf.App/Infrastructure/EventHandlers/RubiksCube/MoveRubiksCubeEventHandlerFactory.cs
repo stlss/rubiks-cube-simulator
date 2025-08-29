70b8b2a5d0b072e98864831583112fda01dc960a
stlss <a.ivakin2014@gmail.com>
2025-08-29 19:15:38 +0500

    [feature/cube-moving] add factory and subscription manager of move cube event handler

---
+++ RubiksCubeSimulator.Wpf.App/Infrastructure/EventHandlers/RubiksCube/MoveRubiksCubeEventHandlerFactory.cs
@@ -0,0 +1,21 @@
+﻿using RubiksCubeSimulator.Wpf.UserControls.ViewModels.RubiksCube;
+
+namespace RubiksCubeSimulator.Wpf.App.Infrastructure.EventHandlers.RubiksCube;
+
+internal interface IMoveRubiksCubeEventHandlerFactory
+{
+    public IReadOnlyList<IMoveRubiksCubeEventHandler> CreateMoveRubiksCubeEventHandlers(
+        RubiksCubeControlViewModel cubeViewModel);
+}
+
+internal sealed class MoveRubiksCubeEventHandlerFactory : IMoveRubiksCubeEventHandlerFactory
+{
+    public IReadOnlyList<IMoveRubiksCubeEventHandler> CreateMoveRubiksCubeEventHandlers(
+        RubiksCubeControlViewModel cubeViewModel)
+    {
+        return new List<IMoveRubiksCubeEventHandler>
+        {
+            new RubiksCubeMoveArrowSetter(cubeViewModel)
+        };
+    }
+}
---
+++ RubiksCubeSimulator.Wpf.App/Infrastructure/EventSubscriptionManagers/MoveRubiksCubeEventSubscriptionManager.cs
@@ -0,0 +1,36 @@
+﻿using RubiksCubeSimulator.Wpf.App.Infrastructure.EventHandlers;
+using RubiksCubeSimulator.Wpf.App.Infrastructure.EventHandlers.RubiksCube;
+
+namespace RubiksCubeSimulator.Wpf.App.Infrastructure.EventSubscriptionManagers;
+
+internal interface IMoveRubiksCubeEventSubscriptionManager
+{
+    public void Subscribe(IMainWindowEventHandler mainWindowViewModelEventHandler,
+        IEnumerable<IMoveRubiksCubeEventHandler> moveCubeEventHandlers);
+
+    public void Unsubscribe(IMainWindowEventHandler mainWindowViewModelEventHandler,
+        IEnumerable<IMoveRubiksCubeEventHandler> cubeEventHandlers);
+}
+
+internal sealed class MoveMoveRubiksCubeEventSubscriptionManager : IMoveRubiksCubeEventSubscriptionManager
+{
+    public void Subscribe(IMainWindowEventHandler mainWindowViewModelEventHandler,
+        IEnumerable<IMoveRubiksCubeEventHandler> moveCubeEventHandlers)
+    {
+        foreach (var moveCubeEventHandler in moveCubeEventHandlers)
+        {
+            mainWindowViewModelEventHandler.MovingRubiksCube += moveCubeEventHandler.OnMovingRubiksCube;
+            mainWindowViewModelEventHandler.MovedRubiksCube += moveCubeEventHandler.OnMovedRubiksCube;
+        }
+    }
+
+    public void Unsubscribe(IMainWindowEventHandler mainWindowViewModelEventHandler,
+        IEnumerable<IMoveRubiksCubeEventHandler> moveCubeEventHandlers)
+    {
+        foreach (var moveCubeEventHandler in moveCubeEventHandlers)
+        {
+            mainWindowViewModelEventHandler.MovingRubiksCube -= moveCubeEventHandler.OnMovingRubiksCube;
+            mainWindowViewModelEventHandler.MovedRubiksCube -= moveCubeEventHandler.OnMovedRubiksCube;
+        }
+    }
+}
