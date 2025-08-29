87daca3fe250cf22e53227a0ff114c29e61e9bb0
stlss <a.ivakin2014@gmail.com>
2025-08-29 19:10:24 +0500

    [feature/cube-moving] add cube control event handler and subscription manager

---
+++ RubiksCubeSimulator.Wpf.App/Infrastructure/EventHandlers/RubiksCubeControlEventHandler.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+using RubiksCubeSimulator.Wpf.UserControls.ViewModels.RubiksCube;
+
+namespace RubiksCubeSimulator.Wpf.App.Infrastructure.EventHandlers;
+
+internal interface IRubiksCubeControlEventHandler
+{
+    public FaceName? FaceName { get; }
+
+    public int? StickerNumber { get; }
+
+    public Point? RelativeMouserPosition { get; }
+
+
+    public void OnRelativeMouseMoveUpFace(object? sender, RelativeMouseMoveRubiksCubeFaceEventArgs e);
+
+    public void OnRelativeMouseMoveRightFace(object? sender, RelativeMouseMoveRubiksCubeFaceEventArgs e);
+
+    public void OnRelativeMouseMoveLeftFace(object? sender, RelativeMouseMoveRubiksCubeFaceEventArgs e);
+}
+
+internal sealed class RubiksCubeControlEventHandler : IRubiksCubeControlEventHandler
+{
+    public FaceName? FaceName { get; private set; }
+
+    public int? StickerNumber { get; private set; }
+
+    public Point? RelativeMouserPosition { get; private set; }
+
+
+    public void OnRelativeMouseMoveUpFace(object? sender, RelativeMouseMoveRubiksCubeFaceEventArgs e)
+    {
+        FaceName = e.RelativeMousePosition != null ? EventHandlers.FaceName.Up : null;
+        StickerNumber = e.StickerNumber;
+        RelativeMouserPosition = e.RelativeMousePosition;
+    }
+
+    public void OnRelativeMouseMoveRightFace(object? sender, RelativeMouseMoveRubiksCubeFaceEventArgs e)
+    {
+        FaceName = e.RelativeMousePosition != null ? EventHandlers.FaceName.Right : null;
+        StickerNumber = e.StickerNumber;
+        RelativeMouserPosition = e.RelativeMousePosition;
+    }
+
+    public void OnRelativeMouseMoveLeftFace(object? sender, RelativeMouseMoveRubiksCubeFaceEventArgs e)
+    {
+        FaceName = e.RelativeMousePosition != null ? EventHandlers.FaceName.Left : null;
+        StickerNumber = e.StickerNumber;
+        RelativeMouserPosition = e.RelativeMousePosition;
+    }
+}
+
+internal enum FaceName
+{
+    Up,
+    Right,
+    Left,
+}
---
+++ RubiksCubeSimulator.Wpf.App/Infrastructure/EventSubscriptionManagers/RubiksCubeControlEventSubscriptionManager.cs
@@ -0,0 +1,28 @@
+﻿using RubiksCubeSimulator.Wpf.App.Infrastructure.EventHandlers;
+using RubiksCubeSimulator.Wpf.UserControls.ViewModels.RubiksCube;
+
+namespace RubiksCubeSimulator.Wpf.App.Infrastructure.EventSubscriptionManagers;
+
+internal interface IRubiksCubeControlEventSubscriptionManager
+{
+    public void Subscribe(RubiksCubeControlViewModel cubeViewModel, IRubiksCubeControlEventHandler cubeEventHandler);
+
+    public void Unsubscribe(RubiksCubeControlViewModel cubeViewModel, IRubiksCubeControlEventHandler cubeEventHandler);
+}
+
+internal sealed class RubiksCubeControlEventSubscriptionManager : IRubiksCubeControlEventSubscriptionManager
+{
+    public void Subscribe(RubiksCubeControlViewModel cubeViewModel, IRubiksCubeControlEventHandler cubeEventHandler)
+    {
+        cubeViewModel.UpFaceViewModel.RelativeMouseMove += cubeEventHandler.OnRelativeMouseMoveUpFace;
+        cubeViewModel.RightFaceViewModel.RelativeMouseMove += cubeEventHandler.OnRelativeMouseMoveRightFace;
+        cubeViewModel.LeftFaceViewModel.RelativeMouseMove += cubeEventHandler.OnRelativeMouseMoveLeftFace;
+    }
+
+    public void Unsubscribe(RubiksCubeControlViewModel cubeViewModel, IRubiksCubeControlEventHandler cubeEventHandler)
+    {
+        cubeViewModel.UpFaceViewModel.RelativeMouseMove -= cubeEventHandler.OnRelativeMouseMoveUpFace;
+        cubeViewModel.RightFaceViewModel.RelativeMouseMove -= cubeEventHandler.OnRelativeMouseMoveRightFace;
+        cubeViewModel.LeftFaceViewModel.RelativeMouseMove -= cubeEventHandler.OnRelativeMouseMoveLeftFace;
+    }
+}
