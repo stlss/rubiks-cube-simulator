53cb5f36027644f7fb677a63fb74e5966f02576e
stlss <a.ivakin2014@gmail.com>
2025-08-29 19:13:03 +0500

    [feature/cube-moving] add interface of move cube event handler

---
+++ RubiksCubeSimulator.Wpf.App/Infrastructure/EventHandlers/RubiksCube/EventArgs/MovedRubiksCubeEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace RubiksCubeSimulator.Wpf.App.Infrastructure.EventHandlers.RubiksCube.EventArgs;
+
+internal sealed class MovedRubiksCubeEventArgs : System.EventArgs
+{
+    public required FaceName FaceName { get; init; }
+
+    public required int StickerNumber { get; init; }
+
+    public required Point RelativeMousePosition { get; init; }
+
+
+    public required Key PressedMoveKey { get; init; }
+
+    public required bool PressedShift { get; init; }
+}
---
+++ RubiksCubeSimulator.Wpf.App/Infrastructure/EventHandlers/RubiksCube/IMoveRubiksCubeEventHandler.cs
@@ -0,0 +1,10 @@
+﻿using RubiksCubeSimulator.Wpf.App.Infrastructure.EventHandlers.RubiksCube.EventArgs;
+
+namespace RubiksCubeSimulator.Wpf.App.Infrastructure.EventHandlers.RubiksCube;
+
+internal interface IMoveRubiksCubeEventHandler
+{
+    public void OnMovingRubiksCube(object? sender, MovingRubiksCubeEventArgs e);
+
+    public void OnMovedRubiksCube(object? sender, MovedRubiksCubeEventArgs e);
+}
