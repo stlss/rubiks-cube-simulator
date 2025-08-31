70c28815805790962fcc57430e4e9d183f77a7df
stlss <a.ivakin2014@gmail.com>
2025-08-31 21:38:53 +0500

    [feature/cube-moving] add IPublisher and ISubscriber in wpf contracts

---
+++ RubiksCubeSimulator.Wpf.Contracts/Events/ISubscriber.cs
@@ -0,0 +1,6 @@
+﻿namespace RubiksCubeSimulator.Wpf.Contracts.Events;
+
+public interface ISubscriber<in T> where T : System.EventArgs
+{
+    public void OnEvent(object sender, T mouseMoveEventArgs);
+}
