e7020e8f88326ebff327a9dc6582519fc910d000
stlss <a.ivakin2014@gmail.com>
2025-09-06 00:57:05 +0500

    [feature/cube-moving] update main window view model - add IPublisher<KeyEventArgs>

--- RubiksCubeSimulator.Wpf.App/ViewModels/Windows/MainWindowViewModel.cs
+++ RubiksCubeSimulator.Wpf.App/ViewModels/Windows/MainWindowViewModel.cs
@@ -2,12 +2,13 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using RubiksCubeSimulator.Wpf.App.Infrastructure;
+using RubiksCubeSimulator.Wpf.Events;
 using RubiksCubeSimulator.Wpf.Infrastructure.RubiksCubeContext;
 using RubiksCubeSimulator.Wpf.UserControls.ViewModels.RubiksCube;
 
 namespace RubiksCubeSimulator.Wpf.App.ViewModels.Windows;
 
-internal sealed class MainWindowViewModel : ObservableObject
+internal sealed class MainWindowViewModel : ObservableObject, IPublisher<KeyEventArgs>
 {
     private readonly IRubiksCubeContext _cubeContext;
 
@@ -23,16 +24,30 @@ internal sealed class MainWindowViewModel : ObservableObject
         var cubeBuilderContextBuilder = serviceProvider.RubiksCubeContextBuilder;
         _cubeContext = cubeBuilderContextBuilder.Build(3);
 
-        KeyDownCommand = new RelayCommand<KeyEventArgs>(e => KeyDown?.Invoke(this, e));
-        KeyUpCommand = new RelayCommand<KeyEventArgs>(e => KeyUp?.Invoke(this, e));
+        KeyDownCommand = new RelayCommand<KeyEventArgs>(keyEventArgs => NotifySubscribers(keyEventArgs!));
+        KeyUpCommand = new RelayCommand<KeyEventArgs>(keyEventArgs => NotifySubscribers(keyEventArgs!));
     }
-
-
-    public event KeyEventHandler? KeyDown;
-    public event KeyEventHandler? KeyUp;
 
 
     public IRelayCommand<KeyEventArgs>? KeyDownCommand { get; private init; }
 
     public IRelayCommand<KeyEventArgs>? KeyUpCommand { get; private init; }
+
+
+    private readonly List<ISubscriber<KeyEventArgs>> _keyEventSubscribers = [];
+
+    public void Subscribe(ISubscriber<KeyEventArgs> subscriber)
+    {
+        _keyEventSubscribers.Add(subscriber);
+    }
+
+    public void Unsubscribe(ISubscriber<KeyEventArgs> subscriber)
+    {
+        _keyEventSubscribers.Remove(subscriber);
+    }
+
+    private void NotifySubscribers(KeyEventArgs keyEventArgs)
+    {
+        foreach (var subscriber in _keyEventSubscribers) subscriber.OnEvent(this, keyEventArgs);
+    }
 }
