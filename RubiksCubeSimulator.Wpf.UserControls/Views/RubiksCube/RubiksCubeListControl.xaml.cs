2652701caca4d59c8963b8fbfac83000b2414808
stlss <a.ivakin2014@gmail.com>
2025-10-08 23:09:41 +0500

    [feature/cube-selecting] add cube list control

---
+++ RubiksCubeSimulator.Wpf.UserControls/Views/RubiksCube/RubiksCubeListControl.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace RubiksCubeSimulator.Wpf.UserControls.Views.RubiksCube;
+
+public sealed partial class RubiksCubeListControl : UserControl
+{
+    public RubiksCubeListControl()
+    {
+        InitializeComponent();
+    }
+}
