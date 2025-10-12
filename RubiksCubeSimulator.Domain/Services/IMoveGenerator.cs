187a8d21eb8d27db8ba169d5b9a7ce57ed46d550
stlss <a.ivakin2014@gmail.com>
2025-10-12 18:32:01 +0500

    [feature/cube-buttons] add move generator

---
+++ RubiksCubeSimulator.Domain/Services/IMoveGenerator.cs
@@ -0,0 +1,8 @@
+﻿using RubiksCubeSimulator.Domain.ValueObjects.RubiksCube.Moves;
+
+namespace RubiksCubeSimulator.Domain.Services;
+
+public interface IMoveGenerator
+{
+    public IReadOnlyList<MoveBase> Generate(int cubeDimension);
+}
