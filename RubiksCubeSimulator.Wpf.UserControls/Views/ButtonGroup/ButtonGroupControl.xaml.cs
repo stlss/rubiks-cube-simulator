b6b4af92624eedd9cd7080f9e64053d1fba47e94
stlss <a.ivakin2014@gmail.com>
2025-10-12 17:18:10 +0500

    [feature/cube-buttons] add button group control

---
+++ RubiksCubeSimulator.Wpf.UserControls/Views/ButtonGroup/ButtonGroupControl.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace RubiksCubeSimulator.Wpf.UserControls.Views.ButtonGroup;
+
+public sealed partial class ButtonGroupControl : UserControl
+{
+    public ButtonGroupControl()
+    {
+        InitializeComponent();
+    }
+}
